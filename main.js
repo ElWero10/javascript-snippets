379023c159979706bcf67e15efde61a08e79a2be
Wero <wero@bluecollarlabs.io>
2025-08-30 19:43:09 -0700

    Add book object constructor
    Added a book object and tested ternary statement

--- main.js
+++ main.js
@@ -19,3 +19,14 @@ const player2 = new Player2("Joe", 1);
 console.log(player2.name)
 console.log(player2.score)
 console.log(player2.sayHello())
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = () => `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "already read" : "not read yet"}.`;
+}
+
+const book1 = new Book("Lord of the Rings", "J.R.R Tolkien", 295, false);
+console.log(book1.info())
